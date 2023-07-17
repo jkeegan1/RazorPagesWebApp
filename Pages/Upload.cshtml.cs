e2cd3a9bdb0995086e6f3c8d7a7c31ae38f2613b
jkeegan1 <joshjkeegan@gmail.com>
2023-07-16 21:31:52 -0400

    add upload page

---
+++ Pages/Upload.cshtml.cs
@@ -0,0 +1,19 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace RazorPagesWebApp.Pages;
+
+public class UploadModel : PageModel
+{
+    private readonly ILogger<UploadModel> _logger;
+
+    public UploadModel(ILogger<UploadModel> logger)
+    {
+        _logger = logger;
+    }
+
+    public void OnGet()
+    {
+    }
+}
+

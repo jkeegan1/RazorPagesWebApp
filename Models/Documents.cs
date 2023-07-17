205683bbbfeb07455d773c5530e0c832615be58d
jkeegan1 <joshjkeegan@gmail.com>
2023-07-16 20:48:33 -0400

    doc model init

---
+++ Models/Documents.cs
@@ -0,0 +1,8 @@
+namespace RazorPagesWebApp.Models;
+
+public class Documents
+{
+    public string? DocTypes { get; set; }
+    public int DocId { get; set; }
+    public DateTime CreationDate { get; set; }
+}

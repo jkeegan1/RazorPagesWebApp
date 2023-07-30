75bc7cc3527165009c6f63a42ec4121b4bc8964d
jkeegan1 <joshjkeegan@gmail.com>
2023-07-29 20:19:56 -0400

    mvc / css updates

--- Models/Documents.cs
+++ Models/Documents.cs
@@ -6,3 +6,9 @@ public class Documents
     public int DocId { get; set; }
     public DateTime CreationDate { get; set; }
 }
+
+public class DocumentModel
+{
+   
+    public string? DocumentUpload { get; set; }
+}
--- Pages/Upload.cshtml.cs
+++ Pages/Upload.cshtml.cs
@@ -15,5 +15,6 @@ public class UploadModel : PageModel
     public void OnGet()
     {
     }
+
 }
 
